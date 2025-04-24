fb6bb84e1b70ca14b56a71b617755662919db4b0
Shyam <gshyamprasad2001@gmail.com>
2025-04-24 13:35:41 +0530

    ...

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { useScroll, motion } from "motion/react";
+import {  motion } from "motion/react";
 
 import {
     Heart,
