e3c64c2eaf98b0c7c5e395a49a9775ea867f8d9e
Shyam <gshyamprasad2001@gmail.com>
2025-04-24 17:36:12 +0530

    Too much gaand fat gayi

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ import Connection from "@/components/Connection";
 import HowitWorks from "@/components/How-it-works";
 import Feauterdcards from "@/components/Feauterdcards";
 import Pricing from "@/components/Pricing";
+import AppFeauters from "@/components/AppFeauters";
+import TestimonialSection from "@/components/Testimonal";
+
 
 export default function Home() {
-  
 
   return (
     <>
@@ -21,6 +23,8 @@ export default function Home() {
           <Feauterdcards />
           <HowitWorks />
           <Pricing />
+          <AppFeauters />
+          <TestimonialSection />
 
         </div>
       </div>
