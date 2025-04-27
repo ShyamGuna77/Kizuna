a3f6e02d25950d26881337f470a73bf23bcc43ba
Shyam <gshyamprasad2001@gmail.com>
2025-04-27 11:41:23 +0530

    Feat :Addex prism amodel

--- app/Lists/page.tsx
+++ app/Lists/page.tsx
@@ -1,9 +1,14 @@
 
 import React from 'react'
+import Container from '@/components/Container'
 
 const Lists = () => {
   return (
-    <div>Lists</div>
+    <Container className="mt-4">
+      <p>
+        Hello thus this COntainer frin lsits
+      </p>
+    </Container>
   )
 }
 
